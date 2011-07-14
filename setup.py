1d1ef7f6f6ab44cab3a23d05cf00f2fe4fb86a32
farshid <farshid.ghods@gmail.com>
2011-07-14 08:48:20 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# chardet's setup.py
+from distutils.core import setup
+setup(
+    name = "pymembase",
+    packages = ["pymembase"],
+    version = "0.0.1",
+    description = "Membase Python SDK",
+    author = "Couchbase Inc",
+    author_email = "info@couchbae.com",
+    url = "http://couchbase.com/",
+    download_url = "http://.../pysdk.tar.gz",
+    keywords = ["encoding", "i18n", "xml"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+Membase Python Library
+his version requires Python 2.6 or later
+"""
+)
