1d32d320276b06bafe58a937042d8e7cccda9a9d
farshid <farshid.ghods@gmail.com>
2011-07-18 02:07:15 -0700

    modified mctest.py
    Usage: mctest.py [options]
    
    Options:
      -h, --help            show this help message and exit
      -n 192.168.1.1:8091, --node=192.168.1.1:8091
                            node's ns_server ip:port
      -u Administrator, --user=Administrator
                            node username
      -p asdasd, --password=asdasd
                            node password
      -b default, --bucket=default
                            which bucket to insert data
      -j True, --json=True  insert json data
      -i 100, --items=100   number of items to be inserted
      --prefix=pymc         prefix to use for memcached keys and json _ids,default
                            is uuid string

--- pymembase/mctest.py
+++ pymembase/mctest.py
@@ -1,16 +1,67 @@
 import uuid
 from membaseclient import VBucketAwareMembaseClient
+from optparse import OptionParser
+from util import ProgressBar
+import sys
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-n", "--node", dest="node",
+                      help="node's ns_server ip:port", metavar="192.168.1.1:8091")
+    parser.add_option("-u", "--user", dest="username",
+                      help="node username", metavar="Administrator", default="Administrator")
+    parser.add_option("-p", "--password", dest="password",
+                      help="node password", metavar="asdasd")
+    parser.add_option("-b", "--bucket", dest="bucket", help="which bucket to insert data",
+                      default="default", metavar="default")
+    parser.add_option("-j", "--json", dest="load_json", help="insert json data",
+                      default=False, metavar="True")
+
+    parser.add_option("-i", "--items", dest="items", help="number of items to be inserted",
+                      default=100, metavar="100")
+
+    parser.add_option("--prefix", dest="key_prefix",
+                      help="prefix to use for memcached keys and json _ids,default is uuid string",
+                      default=str(uuid.uuid4())[:5], metavar="pymc")
+
+    options, args = parser.parse_args()
+
+    node = options.node
+    hostname = node[:node.find(":")]
+    port = node[node.find(":") + 1:]
+    print hostname
+    print port
+    server = {"ip": hostname,
+              "port": port,
+              "rest_username": options.username,
+              "rest_password": options.password,
+              "username": options.username,
+              "password": options.password}
+    print server
+    try:
+        v = VBucketAwareMembaseClient(server, options.bucket)
+        number_of_items = int(options.items)
+        bar = ProgressBar(0, number_of_items, 77)
+        old_bar_string = ""
+        for i in range(0, number_of_items):
+            key = "{0}-{1}".format(options.key_prefix, str(uuid.uuid4())[:5])
+            a, b, c = v.set(key, 0, 0, "hi")
+            a, b, c = v.get(key)
+            bar.updateAmount(i)
+            if old_bar_string != str(bar):
+                sys.stdout.write(str(bar) + '\r')
+                sys.stdout.flush()
+                old_bar_string = str(bar)
+        bar.updateAmount(number_of_items)
+        sys.stdout.write(str(bar) + '\r')
+        sys.stdout.flush()
+        v.done()
+        print ""
+        sys.exit()
+    except KeyboardInterrupt:
+        print "Bye"
+        sys.exit()
+    except Exception as ex:
+        print ex
 
 
-server = {"ip":"172.16.75.136",
-          "port":8091,
-          "rest_username":"Administrator",
-          "rest_password":"password",
-          "username":"Administrator",
-          "password":"password"}
-v = VBucketAwareMembaseClient(server,"default")
-v.memcached("hi").set("hi", 0, 0, "hi")
-for i in range(0, 10000):
-    key = str(uuid.uuid4())
-    a, b, c = v.set(key, 0, 0, "hi")
-    a, b, c = v.get(key)
