6a8daba45cb3aeebfb28202f5d2083c7668d1bf0
Benjamin Young <byoung@bigbluehat.com>
2012-05-18 11:44:19 -0400

    added initial test_restclient.py
    Change-Id: I1066aae122f3389a6ee001069eb74a11abe4b4d8
    Reviewed-on: http://review.couchbase.org/16187
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Benjamin Young <benjamin@couchbase.com>

---
+++ couchbase/tests/test_restclient.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2012, Couchbase, Inc.
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import unittest
+from testconfig import config
+from couchbase.rest_client import RestConnection
+
+
+class RestClientTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.host = config['node-1']['host']
+        self.port = config['node-1']['port']
+        self.username = config['node-1']['username']
+        self.password = config['node-1']['password']
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_rest_client_object_creation(self):
+        server_info = {"ip":self.host,
+                       "port":self.port,
+                       "username":self.username,
+                       "password":self.password}
+        rest = RestConnection(server_info)
+        self.assertEqual(rest.baseUrl, "http://%s:%s/" % (self.host, self.port))
+
+
+if __name__ == "__main__":
+    unittest.main()
