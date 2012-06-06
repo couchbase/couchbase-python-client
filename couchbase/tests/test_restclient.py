8d965ba473776f89f956142f6ad0c8d1dda8edb9
Benjamin Young <byoung@bigbluehat.com>
2012-06-06 13:19:23 -0400

    testing get_view
    Change-Id: Ibdbaa804606c59752791dced8f3674c49302c5bc
    Reviewed-on: http://review.couchbase.org/16882
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Reviewed-by: Volker Mische <volker.mische@gmail.com>
    Tested-by: Benjamin Young <benjamin@couchbase.com>

--- couchbase/tests/test_restclient.py
+++ couchbase/tests/test_restclient.py
@@ -156,6 +156,18 @@ class RestConnectionTest(unittest.TestCase):
         self.teardown_design_doc(ddoc_name)
 
     @attr(cbv="2.0.0")
+    def test_get_view(self):
+        (ddoc_name, resp) = self.setup_create_design_doc()
+        view = self.rest.get_view(self.bucket_name, ddoc_name, "testing")
+        self.assertIn("rows", view.keys())
+        self.teardown_design_doc(ddoc_name)
+        if "error" in view:
+            self.fail(view)
+        else:
+            self.assertIn("rows", view.keys())
+        self.teardown_design_doc(ddoc_name)
+
+    @attr(cbv="2.0.0")
     def test_delete_design_doc(self):
         ddoc_name, resp = self.setup_create_design_doc()
         self.assertTrue(self.rest.delete_design_doc(self.bucket_name,
