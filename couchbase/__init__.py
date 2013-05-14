5ee2a256dc783c7eb9024078fa85538b5387df2f
Mark Nunberg <mnunberg@haskalah.org>
2013-05-13 20:51:14 -0700

    PYCBC-100: Couchbase Apache Copyrights
    Change-Id: I76581c337b5df9aab203431f25eac6ab92631318

--- couchbase/__init__.py
+++ couchbase/__init__.py
@@ -1,3 +1,20 @@
+#
+# Copyright 2013, Couchbase, Inc.
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import couchbase.libcouchbase
 from couchbase.libcouchbase import (
     FMT_UTF8, FMT_BYTES, FMT_JSON, FMT_PICKLE, FMT_MASK)
--- tests/test_set.py
+++ tests/test_set.py
@@ -1,3 +1,20 @@
+#
+# Copyright 2013, Couchbase, Inc.
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from time import sleep
 
 from couchbase import FMT_JSON, FMT_PICKLE, FMT_BYTES, FMT_UTF8
@@ -71,7 +88,7 @@ class ConnectionSetTest(CouchbaseTestCase):
         rv7 = self.cb.set('key_format7', b"\x42".decode('utf-8'),
                           format=FMT_UTF8)
         self.assertTrue(rv7.success)
-        
+
 
     def test_set_objects(self):
         for v in (None, False, True):
