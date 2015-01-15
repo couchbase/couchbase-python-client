d336b7ea29a8682a34a434e3ced9f8c4860b39da
Mark Nunberg <mnunberg@haskalah.org>
2015-01-15 10:23:57 -0800

    Modify version generation to conform with PEP-440
    Change-Id: Ie4f7cea13f6d96605ab1ef79dc349bae32a3d04f
    Reviewed-on: http://review.couchbase.org/45389
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Volker Mische <volker.mische@gmail.com>

--- couchbase_version.py
+++ couchbase_version.py
@@ -1,26 +1,86 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import subprocess
 import datetime
-import sys
 import os.path
 import warnings
+import re
 
-class CantInvokeGit(Exception): pass
-class VersionNotFound(Exception): pass
 
-verfile = os.path.join(
+class CantInvokeGit(Exception):
+    pass
+
+
+class VersionNotFound(Exception):
+    pass
+
+
+RE_XYZ = re.compile(r'(\d+)\.(\d+)\.(\d)(?:-(.*))?')
+
+VERSION_FILE = os.path.join(
     os.path.dirname(__file__),
     os.path.join("couchbase", "_version.py"))
+
+
+class VersionInfo(object):
+    def __init__(self, rawtext):
+        self.rawtext = rawtext
+        vinfo, self.ncommits, self.sha = rawtext.rsplit('-', 2)
+        self.ncommits = int(self.ncommits)
+
+        # Split up the X.Y.Z
+        match = RE_XYZ.match(vinfo)
+        (self.ver_maj, self.ver_min, self.ver_patch, self.ver_extra) =\
+            match.groups()
+
+        # Per PEP-440, replace any 'DP' with an 'a', and any beta with 'b'
+        if self.ver_extra:
+            self.ver_extra = re.sub(r'^dp', 'a', self.ver_extra, count=1)
+            self.ver_extra = re.sub(r'^beta', 'b', self.ver_extra, count=1)
+            m = re.search(r'^([ab])(\d+)?', self.ver_extra)
+            if m.group(2) is None:
+                # No suffix, then add the number
+                first = self.ver_extra[0]
+                self.ver_extra = first + '0' + self.ver_extra[1:]
+
+    @property
+    def is_final(self):
+        return self.ncommits == 0
+
+    @property
+    def is_prerelease(self):
+        return self.ver_extra
+
+    @property
+    def xyz_version(self):
+        return '.'.join((self.ver_maj, self.ver_min, self.ver_patch))
+
+    @property
+    def base_version(self):
+        """Returns the actual upstream version (without dev info)"""
+        components = [self.xyz_version]
+        if self.ver_extra:
+            components.append(self.ver_extra)
+        return ''.join(components)
+
+    @property
+    def package_version(self):
+        """Returns the well formed PEP-440 version"""
+        vbase = self.base_version
+        if self.ncommits:
+            vbase += '.dev{0}+{1}'.format(self.ncommits, self.sha)
+        return vbase
+
 
 def get_version():
     """
     Returns the version from the generated version file without actually
     loading it (and thus trying to load the extension module).
     """
-    if not os.path.exists(verfile):
-        raise VersionNotFound(verfile + " does not exist")
-    fp = open(verfile, "r")
+    if not os.path.exists(VERSION_FILE):
+        raise VersionNotFound(VERSION_FILE + " does not exist")
+    fp = open(VERSION_FILE, "r")
     vline = None
     for x in fp.readlines():
         x = x.rstrip()
@@ -36,20 +96,15 @@ def get_version():
 
     return vline.strip().rstrip().replace("'", '')
 
-def gen_version():
-    """
-    Generate a version based on git tag info. This will write the
-    couchbase/_version.py file. If not inside a git tree it will raise a
-    CantInvokeGit exception - which is normal (and squashed by setup.py) if
-    we are running from a tarball
-    """
+
+def get_git_describe():
     if not os.path.exists(os.path.join(os.path.dirname(__file__), ".git")):
         raise CantInvokeGit("Not a git build")
 
     try:
-        po = subprocess.Popen(("git", "describe", "--tags", "--long", "--always"),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+        po = subprocess.Popen(
+            ("git", "describe", "--tags", "--long", "--always"),
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     except OSError as e:
         raise CantInvokeGit(e)
 
@@ -57,46 +112,57 @@ def gen_version():
     if po.returncode != 0:
         raise CantInvokeGit("Couldn't invoke git describe", stderr)
 
-    try:
-        # Python 3
-        stdout = str(stdout.rstrip(), 'utf-8')
-    except TypeError:
-        stdout = str(stdout.rstrip())
+    return stdout.decode('utf-8').rstrip()
 
-    info = stdout.split('-')
-    sha1 = info[-1]
-    try:
-        ncommits = int(info[-2])
-        basevers = '-'.join(info[:-2])
-        # Make the version string itself
-        if not ncommits:
-            vstr = basevers
-        else:
-            vstr = stdout
 
+def gen_version(do_write=True, txt=None):
+    """
+    Generate a version based on git tag info. This will write the
+    couchbase/_version.py file. If not inside a git tree it will
+    raise a CantInvokeGit exception - which is normal
+    (and squashed by setup.py) if we are running from a tarball
+    """
+
+    if txt is None:
+        txt = get_git_describe()
+
+    try:
+        info = VersionInfo(txt)
+        vstr = info.package_version
     except IndexError:
-        warnings.warn("Malformed tag '{0}'".format(stdout))
-        vstr = "0.0.0-UNKNOWN-" + stdout
+        warnings.warn("Malformed input '{0}'".format(txt))
+        vstr = '0.0.0'+txt
 
-    fp = open(verfile, "w")
-    fp.write('''
-# This file automatically generated by
-#   {path}
-# at
-#   {now}
-__version__ = '{vstr}'
-'''.format(path=__file__,
-           now=datetime.datetime.now().isoformat(' '),
-           vstr=vstr))
+    if not do_write:
+        print(vstr)
+        return
 
-    fp.close()
+    lines = (
+        '# This file automatically generated by',
+        '#    {0}'.format(__file__),
+        '# at',
+        '#    {0}'.format(datetime.datetime.now().isoformat(' ')),
+        "__version__ = '{0}'".format(vstr)
+    )
+    with open(VERSION_FILE, "w") as fp:
+        fp.write("\n".join(lines))
 
-if __name__ == "__main__":
-    cmd = sys.argv[1]
-    if cmd == "show":
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    ap = ArgumentParser(description='Parse git version to PEP-440 version')
+    ap.add_argument('-c', '--mode', choices=('show', 'make', 'parse'))
+    ap.add_argument('-i', '--input',
+                    help='Sample input string (instead of git)')
+    options = ap.parse_args()
+
+    cmd = options.mode
+    if cmd == 'show':
         print(get_version())
-    elif cmd == "make":
-        gen_version()
+    elif cmd == 'make':
+        gen_version(do_write=True, txt=options.input)
         print(get_version())
+    elif cmd == 'parse':
+        gen_version(do_write=False, txt=options.input)
+
     else:
         raise Exception("Command must be 'show' or 'make'")
