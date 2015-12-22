dccba89bb07c17f8365567ac41c14bdf07f87088
Louis-Philippe Huberdeau <lp@huberdeau.info>
2015-12-22 13:20:09 -0500

    Improve conditional loading of tests

--- acouchbase/tests/asyncio_tests.py
+++ acouchbase/tests/asyncio_tests.py
@@ -1,9 +1,16 @@
+# SyntaxError will trigger if yield or async is not supported
+# ImportError will fail for python 3.3 because asyncio does not exist
+
 try:
     from py34only import CouchBaseTest
+except ImportError:
+    pass
 except SyntaxError:
     pass
 
 try:
     from py35only import CouchBasePy35Test
+except ImportError:
+    pass
 except SyntaxError:
     pass
