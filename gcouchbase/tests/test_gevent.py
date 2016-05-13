e35373ee56f47f7bbbdd7f21a286d6784b98360b
Heungsub Lee <sub@subl.ee>
2016-05-13 11:26:02 +0900

    Fix PYCBC-344 when timed out
    ----
    Clear async result callbacks always
    ----
    Make test case of result confusion when timed out
    
    Change-Id: I4f924d0105bef63894577b083b6fe95516ef49d1
    Reviewed-on: http://review.couchbase.org/64014
    Reviewed-by: Mark Nunberg <mark.nunberg@couchbase.com>
    Tested-by: Mark Nunberg <mark.nunberg@couchbase.com>

--- gcouchbase/tests/test_gevent.py
+++ gcouchbase/tests/test_gevent.py
@@ -1,3 +1,5 @@
+from itertools import cycle
+
 from couchbase.tests.base import SkipTest
 try:
     import gevent
@@ -13,30 +15,47 @@ class GeventSpecificTest(ConnectionTestCase):
     viewfactory = GView
     should_check_refcount = False
 
+    def setUp(self):
+        super(GeventSpecificTest, self).setUp()
+        self.foo = self.gen_key('foo')
+        self.bar = self.gen_key('bar')
+        self.cb.upsert(self.foo, 'foo')
+        self.cb.upsert(self.bar, 'bar')
+
     def test_killing_greenlet(self):
-        foo = self.gen_key('foo')
-        bar = self.gen_key('bar')
-        self.cb.upsert(foo, 'foo')
-        self.cb.upsert(bar, 'bar')
         def load_foo_and_bar_forever():
-            while True:
+            for key, value in cycle([(self.foo, 'foo'), (self.bar, 'bar')]):
                 try:
-                    r = self.cb.get(foo)
+                    r = self.cb.get(key)
                 except gevent.GreenletExit:
                     continue
                 else:
-                    self.assertEqual(r.value, 'foo')
-                try:
-                    r = self.cb.get(bar)
-                except gevent.GreenletExit:
-                    continue
-                else:
-                    self.assertEqual(r.value, 'bar')
+                    self.assertEqual(r.value, value)
         def kill_greenlet_forever(g):
             while True:
                 gevent.sleep(0.1)
                 g.kill()
         g = gevent.spawn(load_foo_and_bar_forever)
         greenlets = [g, gevent.spawn(kill_greenlet_forever, g)]
-        with gevent.Timeout(1, False):
-            gevent.joinall(greenlets, raise_error=True)
+        try:
+            with gevent.Timeout(1, False):
+                gevent.joinall(greenlets, raise_error=True)
+        finally:
+            gevent.killall(greenlets, StopIteration)
+
+    def test_timeout(self):
+        def load_foo_and_bar_forever():
+            for key, value in cycle([(self.foo, 'foo'), (self.bar, 'bar')]):
+                try:
+                    with gevent.Timeout(0.0000001):
+                        r = self.cb.get(key)
+                except gevent.Timeout:
+                    continue
+                else:
+                    self.assertEqual(r.value, value)
+        g = gevent.spawn(load_foo_and_bar_forever)
+        try:
+            with gevent.Timeout(1, False):
+                g.get()
+        finally:
+            g.kill(StopIteration)
