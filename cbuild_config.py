b748a3ed43adcfecd386a20220fcfba71438e209
Ellis Breen <ellis.breen@couchbase.com>
2020-01-21 10:12:27 +0000

    PYCBC-741: Bump LCB major version for Linux
    Change-Id: I9688eb77527b5491061bb5eaa7c8caff3ba35edd
    Reviewed-on: http://review.couchbase.org/120890
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Ellis Breen <ellis.breen@couchbase.com>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- cbuild_config.py
+++ cbuild_config.py
@@ -177,7 +177,7 @@ class CBuildInfo:
         plat = get_plat_code()
 
         print("Got platform {}".format(plat))
-        default = ['libcouchbase.so.5']
+        default = ['libcouchbase.so.6']
         return {'darwin': ['libcouchbase.2.dylib', 'libcouchbase.dylib'], 'linux': default,
                 'win': ['libcouchbase_d.dll','libcouchbase.dll']}.get(get_plat_code(), default)
 
